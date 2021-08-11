f1139aa4a472c79338792420d03861d699bb66e7
Phong <dinhphong72@gmail.com>
2021-08-11 14:10:14 +0700

    tutorial_add_javascript

---
+++ tutorial_add_javascript/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "tutorial_add_javascript",
+    'summary': """Example Hello World application""",
+    'description': """This is an example application to learn the basics of the JS framework.""",
+    'author': "An Awesome Developer",
+    'version': '14.0.0.1',
+    'depends': ['base', 'web'],
+    'application': True,
+    'data': [
+     'views/assets.xml',
+    ],
+}
