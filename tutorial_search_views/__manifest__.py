6e16664fa055a4c45c5b6998d7790f4c4702415c
Phong <dinhphong72@gmail.com>
2021-08-13 18:04:08 +0700

    Add Search Views

---
+++ tutorial_search_views/__manifest__.py
@@ -0,0 +1,17 @@
+# __manifest__.py
+
+{
+    "name": "Tutorial Search Views",
+    "summary": "Creating search views with filters/groupby",
+    "description": "Creating search views with filters/groupby",
+    "author": "Oocademy",
+    "website": "http://www.oocademy.com",
+    "category": "Techical/Tutorials",
+    "version": "14.0.0.1",
+    "application": True,
+    "depends": ['contacts'],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+
+}
