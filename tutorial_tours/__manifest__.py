dc1f359633b93b7fe5e477c39dfdb0ff5bf760a5
dinhphong <dinhphong72@gmail.com>
2021-08-15 20:51:30 +0700

    Add Web tour

---
+++ tutorial_tours/__manifest__.py
@@ -0,0 +1,16 @@
+# __manifest__.py
+
+{
+    "name": "Tutorial Tours",
+    "summary": "Tutorial Tours",
+    "description": "Tutorial Tours",
+    "author": "Oocademy",
+    "website": "http://www.oocademy.com",
+    "category": "Tutorials/Tours",
+    "version": "14.0.0.1",
+    'depends': ['base', 'contacts', 'web'],
+    "application": True,
+    'data': [
+        'views/assets.xml',
+    ],
+}
---
+++ tutorial_tours/static/src/js/my_tours.js
@@ -0,0 +1,43 @@
+odoo.define('tutorial_creating_tours.example_tour', function(require) {
+    "use strict";
+
+    // Minimal requirements needed to create a tour
+    var core = require('web.core');
+    var tour = require('web_tour.tour');
+
+    // Allows you to translate tour steps
+    var _t = core._t;
+
+    tour.register('example_tour', {
+        url: "/web",
+        // You can find all step options at:
+        //     https://github.com/odoo/odoo/blob/master/addons/web_tour/static/src/js/tour_step_utils.js
+        //
+        // - showAppsMenuItem(): shows a tour blinker on the app that you mention.
+        // - toggleHomeMenu(): shows a tour blinker to go back to the zhome screen of Odoo.
+    }, [tour.stepUtils.showAppsMenuItem(), {
+        trigger: '.o_app[data-menu-xmlid="contacts.menu_contacts"]',
+        content: _t('Want to manage your contacts? It starts here.'),
+        position: 'bottom',
+    }, {
+        trigger: '.o-kanban-button-new',
+        extra_trigger: '.o_res_partner_kanban',
+        content: _t('Let\'s create your first contact by clicking on create.'),
+        position: 'bottom',
+        width: 200,
+    }, {
+        // The trigger will tell that you would like to input a value in the field 'Name'
+        trigger: 'input[placeholder="Name"]',
+        extra_trigger: '.o_form_editable',
+        // This is the 'hint' / tooltip that is shown to the end user
+        content: _t('Fill in the name of the contact.'),
+        // When you run the test (from the developer tools) it will automatically fill in 'James Cook'.
+        run: 'text James Cook',
+        position: 'right',
+    }, {
+        trigger: '.o_form_button_save',
+        extra_trigger: '.oe_avatar',
+        content: _t('Click on save to save your new contact. You can use this contact throughout Odoo later on.'),
+        position: 'bottom',
+    }]);
+});
