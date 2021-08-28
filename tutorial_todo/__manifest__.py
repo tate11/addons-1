4638321c469d17e46bfd9e2fad6e35024e11508a
dinhphong <dinhphong72@gmail.com>
2021-08-28 10:57:07 +0700

    Javascript and Widget

--- tutorial_todo/__manifest__.py
+++ tutorial_todo/__manifest__.py
@@ -9,7 +9,7 @@
     "category": "Tutorials/All",
     "version": "14.0.0.1",
     "application": True,
-    'depends': ['base','contacts','sale','mail'],
+    'depends': ['base','web','contacts','sale','mail'],
     "data": [
         # Data
         #"data/email_template_welcome.xml",
@@ -24,11 +24,16 @@
         "views/res_partner_views.xml",
         "views/configuration_view.xml",
         "views/configuration_menu.xml",
+        "views/tutorial_quiz_assets.xml",
+        "views/tutorial_quiz_views.xml",
 
         # Report
         "report/tutorial_todo_report.xml",
         "report/tutorial_todo_report_templates.xml",
+    ],
 
+    'qweb': [
+        'static/src/xml/tutorial_quiz.xml',
     ],
 
 }
